aa2c8151fef3cad27134ec6d833bec4ae975618c
Sávio Xavier <savioxavier@savios-macbook-pro.local>
2016-07-21 12:57:24 -0300

    Initial Commit

---
+++ OpenWeatherIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenWeatherIOS
+//
+//  Created by Sávio Xavier on 7/21/16.
+//  Copyright © 2016 Sávio Xavier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
